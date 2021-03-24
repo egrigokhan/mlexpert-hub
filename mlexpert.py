dd1ab0b31b74524518a29c38c04dd7dd1d908c73
Gokhan Egri <egrigokhan@gmail.com>
2021-03-24 22:17:46 +0300

    Update mlexpert.py

--- mlexpert.py
+++ mlexpert.py
@@ -12,7 +12,30 @@ import zipfile
 from urllib.request import urlopen, Request
 from urllib.parse import urlparse  # noqa: F401
 
-from util import import_module, _remove_if_exists, _git_archive_link, _check_module_exists
+# Copied from tools/shared/module_loader to be included in torch package
+def import_module(name, path):
+    import importlib.util
+    from importlib.abc import Loader
+    spec = importlib.util.spec_from_file_location(name, path)
+    module = importlib.util.module_from_spec(spec)
+    assert isinstance(spec.loader, Loader)
+    spec.loader.exec_module(module)
+    return module
+
+def _remove_if_exists(path):
+    if os.path.exists(path):
+        if os.path.isfile(path):
+            os.remove(path)
+        else:
+            shutil.rmtree(path)
+
+
+def _git_archive_link(repo_owner, repo_name, branch):
+    return 'https://github.com/{}/{}/archive/{}.zip'.format(repo_owner, repo_name, branch)
+
+def _check_module_exists(name):
+    import importlib.util
+    return importlib.util.find_spec(name) is not None
 
 from tqdm.auto import tqdm  # automatically select proper tqdm submodule if available
 
